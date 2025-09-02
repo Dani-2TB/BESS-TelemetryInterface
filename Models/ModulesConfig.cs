2fa02a53d7cf9940efc66d72f1a93d70ece65513
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2025-09-01 22:48:43 -0400

    fixed model relationship

--- Models/ModulesConfig.cs
+++ Models/ModulesConfig.cs
@@ -4,9 +4,10 @@ namespace DotnetAPI.Models;
 
 public class ConfigBess{
     public Guid Id {get; set; }
-    public required Module ModuleId { get; set; }
+
+    public required Guid ModuleId { get; set; }
+    public Module Module { get; set; } = null!;
+
     public int MaxDCCurrent {get; set; }
     public int MinDCCurrent {get; set; }
-
-
 }
