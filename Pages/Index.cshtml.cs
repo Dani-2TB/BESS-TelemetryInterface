4ca7c582bd794d9a6ff7624f28482e9015833996
Dani Ocaranza <96756521+Dani-0TB@users.noreply.github.com>
2025-09-29 19:10:39 -0300

    razor update namespace

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPages.Pages;
+namespace DotnetAPI.Pages;
 
 public class IndexModel : PageModel
 {
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPages.Pages;
+namespace DotnetAPI.Pages;
 
 public class PrivacyModel : PageModel
 {
